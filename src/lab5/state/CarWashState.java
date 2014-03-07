359dfe19d7387a87db39be404a6962246aa3c2ff
mRossa <malalg-3@student.ltu.se>
2014-03-07 10:24:12 +0100

    Updated CarWashState

--- src/lab5/state/CarWashState.java
+++ src/lab5/state/CarWashState.java
@@ -1,15 +1,18 @@
 package lab5.state;
 
+import lab5.main.Simulator;
 import lab5.random.*;
 
 public class CarWashState extends SimState{
-	public static final int max = Simulator.queueSize;
+
+	public static int max;
 	public static int currentstate;
 	private static long seed = Simulator.seed;
 	
-	private static UniformRandomStream fastRandom; //LAGT TILL
-	private static UniformRandomStream slowRandom;
-	private static ExponentialRandomStream exRandom;
+	public static UniformRandomStream fastRandom; //LAGT TILL
+	public static UniformRandomStream slowRandom;
+	public static ExponentialRandomStream exRandom;
+	private static UniformRandomStream Time;
 	
 	public FIFO queue = new FIFO();
 	public CarFactory factory = new CarFactory();
@@ -20,9 +23,13 @@ public class CarWashState extends SimState{
 		return currentstate;
 	}
 	
-	public CarWashState(int queueSize, int fastMachines, int slowMachines, double fastLow, double fastHigh, double slowLow, double slowHigh, double lambda, long seed){
-		
+	public CarWashState(CarWashState machine) {
+		super(machine);
+		// TODO Auto-generated constructor stub
 	}
+//	public CarWashState(int queueSize, int fastMachines, int slowMachines, double fastLow, double fastHigh, double slowLow, double slowHigh, double lambda, long seed){
+//		
+//	}
 	public static void fast(int fastMachines, double fastLow, double fastHigh){
 		while(fastMachines != 0 ){
 			fastRandom = getTime(fastLow, fastHigh);
@@ -42,9 +49,11 @@ public class CarWashState extends SimState{
 		return false;
 	}
 	
-	public float getTime(){
-		return (float) 0.434;
+	public static UniformRandomStream getTime(double low, double high){
+		Time = RandomStreams.washMachineTime(low, high, seed);
+		return Time;
 	}
+	
 	
 
 	
