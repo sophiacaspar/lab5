2876666ceb3208adc7d1d12c105d1a4b06ec2319
mRossa <malalg-3@student.ltu.se>
2014-03-07 09:56:45 +0100

    added some code

--- state/CarWashState.java
+++ state/CarWashState.java
@@ -3,12 +3,13 @@ package lab5.state;
 import lab5.random.*;
 
 public class CarWashState extends SimState{
-	public static final int max = 5;
+	public static final int max = Simulator.queueSize;
 	public static int currentstate;
+	private static long seed = Simulator.seed;
 	
-	private UniformRandomStream fastRandom; //LAGT TILL
-	private UniformRandomStream slowRandom;
-	private ExponentialRandomStream exRandom;
+	private static UniformRandomStream fastRandom; //LAGT TILL
+	private static UniformRandomStream slowRandom;
+	private static ExponentialRandomStream exRandom;
 	
 	public FIFO queue = new FIFO();
 	public CarFactory factory = new CarFactory();
@@ -22,7 +23,18 @@ public class CarWashState extends SimState{
 	public CarWashState(int queueSize, int fastMachines, int slowMachines, double fastLow, double fastHigh, double slowLow, double slowHigh, double lambda, long seed){
 		
 	}
-	
+	public static void fast(int fastMachines, double fastLow, double fastHigh){
+		while(fastMachines != 0 ){
+			fastRandom = getTime(fastLow, fastHigh);
+			fastMachines --;
+		}
+	}
+	public static void slow(int slowMachines, double slowLow, double slowHigh){
+		while(slowMachines != 0 ){
+			slowRandom = getTime(slowLow, slowHigh);
+			slowMachines --;
+		}
+	}
 	public boolean checkWashing(){
 		if(4== max){
 			return true;
--- state/SimState.java
+++ state/SimState.java
@@ -2,11 +2,13 @@ package lab5.state;
 
 import java.util.Observable;
 
+import lab5.main.Simulator;
+
 public class SimState extends Observable{
 	
 	
 	public final static int start=1, leave=2, stop=0; 
-	public static int rejected=0, arrive=0;
+	public static int rejected=0, arrive=0, number =0, numbers=0;
 	public boolean isEmpty;
 	public CarWashState fast;
 	public CarWashState slow;
@@ -14,6 +16,23 @@ public class SimState extends Observable{
 	
 	public int getState(){
 		return currentState;
+	}
+	public SimState(CarWashState machine){
+		if(machine == fast){
+			while(number != Simulator.fastMachines){
+				CarWashState.fast(Simulator.fastMachines, Simulator.fastHigh, Simulator.fastLow);
+				number ++;
+			}
+			machine = slow;
+			
+		}
+		else if (machine == slow){
+			while(numbers != Simulator.slowMachines){
+				CarWashState.slow(Simulator.slowMachines, Simulator.slowHigh, Simulator.slowLow);
+				numbers ++;
+			}
+		}
+		
 	}
 	
 }
