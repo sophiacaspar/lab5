b83093db9c50ed39c49f5c3cec6d21cab48434fc
sophiacaspar <sophiacaspar@hotmail.com>
2014-03-07 10:22:02 +0100

    added output in carwashstate

--- view/CarWashView.java
+++ view/CarWashView.java
@@ -13,12 +13,24 @@ public class CarWashView implements Observer{
 		currentState = CarWashState.currentstate;
 	}
 	
-	public String output(){
-		return "";
+	public void firstOutput(){
+		System.out.println("-----------------------------------------------------");
+		System.out.println("Time" + "\t" + "Fast" + "\t"
+				+ "Slow" + "\t" + "ID" + "\t" + "Event" + "\t"
+				+ "IdleTime" + "\t" + "QueueTime" + "\t" + "QueueSize"
+				+ "\t" + "Rejected");
+	}
+	
+	public void updateOutput(){
+		
 	}
 
 	public void update(Observable o, Object arg) {
-		
+		boolean firstLine = true;
+		if (firstLine) {
+			firstOutput();
+			firstLine = false;
+			}
 		
 	}
 
