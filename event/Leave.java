f014933e3d02769d144cafce7a125b1f0840cfdd
emakk <emmcar-2@student.ltu.se>
2014-03-07 11:00:51 +0100

    Update Leave.java

--- event/Leave.java
+++ event/Leave.java
@@ -1,7 +1,12 @@
 package lab5.event;
 
+import lab5.state.CarWashState;
+import lab5.state.SimState;
+
 public class Leave {
+	
 	public void execute(){
+		CarWashState.currentstate = SimState.leave;
 		
 	}
 }
