5ae1ddea67c4bf0c4e323d497ff0960b6e0a3af9
sophiacaspar <sophiacaspar@hotmail.com>
2014-03-07 17:22:23 +0100

    Added class massage, testing

---
+++ event/Message.java
@@ -0,0 +1,16 @@
+package lab5.event;
+
+public class Message {
+		double time;
+		int mashineFast;
+		int mashineSlow;
+		String CarID;
+		String currentEvent;
+		double idleTime;
+		double queueTime;
+		int queueSize;
+		int rejected;
+		int queued;
+
+
+}
