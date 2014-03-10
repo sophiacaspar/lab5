bdcc82dcfa51f41d5a3cf618899b63bca0089f2e
mRossa <malalg-3@student.ltu.se>
2014-03-10 17:59:40 +0100

    Update SimState.java

--- state/SimState.java
+++ state/SimState.java
@@ -10,7 +10,7 @@ public class SimState extends Observable{
 	
 	
 	public final static int start=1, leave=2, stop=0; 
-	public static int rejected=0, arrive=0, numberSlow =Simulator.fastMachines, numberFast=Simulator.slowMachines; 
+	public static int numberSlow =Simulator.fastMachines, numberFast=Simulator.slowMachines; 
 	public boolean isEmpty;	
 	public CarWashState fast;
 	public CarWashState slow;
