9137dbe351e3f4747cb85232023937cdb27079b8
sophiacaspar <sophiacaspar@hotmail.com>
2014-03-11 15:04:14 +0100

    Ändrade i main

--- main/Simulator.java
+++ main/Simulator.java
@@ -5,19 +5,21 @@ import lab5.state.CarWashState;
 import lab5.view.CarWashView;
 
 public class Simulator {
+	
+	public static int fastMachines = 2;
+	public static int slowMachines = 2;
+	public static double fastLow = 2.8;
+	public static double fastHigh = 4.6;
+	public static double slowLow = 3.5;
+	public static double slowHigh = 6.7;
+	public static double lambda = 2.0;
+	public static long seed = 1234;
+	public static int queueSize = 5;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int fastMachines = 2;
-		int slowMachines = 2;
-		double fastLow = 2.8;
-		double fastHigh = 4.6;
-		double slowLow = 3.5;
-		double slowHigh = 6.7;
-		double lambda = 2.0;
-		long seed = 1234;
-		int queueSize = 5;
+
 		
 		CarWashState carWashState = new CarWashState(queueSize, fastMachines, slowMachines, fastLow, fastHigh, slowLow, slowHigh, lambda, seed);
 		
