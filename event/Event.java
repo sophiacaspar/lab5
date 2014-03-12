b857ae2b38cd4bf7b90f3609763f1975579e7c86
mRossa <malalg-3@student.ltu.se>
2014-03-12 10:26:18 +0100

    Update Event.java

--- event/Event.java
+++ event/Event.java
@@ -2,30 +2,34 @@ package lab5.event;
 
 import java.util.Observable;
 
-import lab5.state.SimState;
+import lab5.state.CarWashState;
+
 
 public abstract class Event extends Observable {
-	private double time;
-	
+	private static double times= 0.00;
+
 	/**
 	 * @param time
 	 *  creates time that the event class can use.
 	 */
 	public Event(double time){
-		this.time = time;
+		times = time;
 	}
 	/**
 	 * Called when the event happens. 
-	 * @param simState current state of the simulator.
+	 * @param carWashState current state of the simulator.
 	 * @param sortSeq, SortedSequence that executes the event.
 	 */
-	public abstract void execute(SimState simState, SortedSequence sortSeq);
+	public abstract void execute(CarWashState simState, SortedSequence sortSeq);
 	/**
 	 * 
 	 * @return time when event happens
 	 */
 	public double getTime(){
-		return time;
+		return times;
 	}
-}
+	public static double getsTime(){
+		return times;
+	}
 
+}
