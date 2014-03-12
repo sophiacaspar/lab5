eb17c41367161b672dca85d12272ecdcb2140c60
mRossa <malalg-3@student.ltu.se>
2014-03-12 10:21:34 +0100

    Update Simulator.java

--- main/Simulator.java
+++ main/Simulator.java
@@ -1,42 +1,44 @@
 package lab5.main;
 
 /**
- * This program simulates a car wash.
+ * Detta program simmulerar en biltvÃ¤tt.
  */
 
-import lab5.event.*;
-import lab5.state.CarWashState;
+import java.util.Observable;
+
+import lab5.event.Event;
+import lab5.event.SortedSequence;
+import lab5.event.Start;
+import lab5.state.*;
 import lab5.view.CarWashView;
 
-public class Simulator {
+public class Simulator extends Observable {
 
-	/**
-	 * Here we set some variables which is used on other parts of the code.
-	 */
-
-	public static int fastMachines = 2;
-	public static int slowMachines = 2;
-	public static double fastLow = 2.8;
-	public static double fastHigh = 4.6;
-	public static double slowLow = 3.5;
-	public static double slowHigh = 6.7;
-	public static double lambda = 2.0;
-	public static long seed = 1234;
-	public static int queueSize = 5;
-
-	public static void main(String[] args) {
-
-		CarWashState carWashState = new CarWashState(queueSize, fastMachines,
-				slowMachines, fastLow, fastHigh, slowLow, slowHigh, lambda,
-				seed);
-
-		SortedSequence sortSeq = new SortedSequence(carWashState);
+		public static int fastMachines = 2;
+		public static int slowMachines = 2;
+		public static double fastLow = 2.8;
+		public static double fastHigh = 4.6;
+		public static double slowLow = 3.5;
+		public static double slowHigh = 6.7;
+		public static double lambda = 2.0;
+		public static long seed = 1234;
+		public static int queueSize = 5;
+		public static double late = 15.00;
 		
-		CarWashView carWashView = new CarWashView();
+		public Event event;
+		public static SortedSequence sortSeq;
+		public static CarWashState carWashState;
 		
-		carWashState.addObserver(carWashView); //Starts addObserver.
-		sortSeq.addToQueue(new Stop(15)); //Determines for how long the simulation will run.
-		sortSeq.startQueue(new Start(0)); //Starts the simulation.
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			carWashState = new CarWashState(queueSize, fastMachines, slowMachines, fastLow, fastHigh, slowLow, slowHigh, lambda, seed);
+			sortSeq = new SortedSequence(carWashState);
+	    	CarWashView carWashView = new CarWashView();
+	    	carWashState.addObserver(carWashView);
+	    	sortSeq.startQueue(new Start(0));
+		}
 	}
+
 
 }
