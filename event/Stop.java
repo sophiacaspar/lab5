a0d978ae6f03e89b2dacdac6bc8351822a497d92
mRossa <malalg-3@student.ltu.se>
2014-03-12 10:36:26 +0100

    Update Stop.java

--- event/Stop.java
+++ event/Stop.java
@@ -1,28 +1,27 @@
 package lab5.event;
 
 import lab5.state.CarWashState;
-import lab5.state.SimState;
+
 
 public class Stop extends Event{
-
-	/**
-	 * Adds time when to execute (quit)
-	 * @param time
-	 */
-	public Stop(double time) {
-		super(time);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void execute(SimState simState, SortedSequence sortSeq) {
-		CarWashState cwState = (CarWashState)simState;
-		sortSeq.stop();
-		cwState.setMessege(this.getTime());
-		cwState.setChange(this, "-");
-	}
-	
-	public String toString(){
-		return "Stop";
-	}
-}
+		double time = 0.00;
+		/**
+	 	 * Adds time when to execute (quit)
+	 	 * @param time
+	 	 */
+	 	public Stop(double time) {
+	 		super(time);
+	 		this.time = time;
+	 		// TODO Auto-generated constructor stub
+	 	}
+	  
+	 	public void execute(CarWashState cwState, SortedSequence sortSeq) {
+	 		Message.currentEvent = this.toString();
+	 		cwState.setMessege(time);
+	 		cwState.setChange(this, "-");
+	 		sortSeq.stop();
+	  	}
+	 	public String toString(){
+			return "Stop";
+		}
+	 }
