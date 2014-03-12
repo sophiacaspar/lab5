c8aac51c87b74620e3b80816f7da176481701bb4
emakk <emmcar-2@student.ltu.se>
2014-03-12 10:37:10 +0100

    Update Simulator.java

--- main/Simulator.java
+++ main/Simulator.java
@@ -13,33 +13,34 @@ import lab5.state.*;
 import lab5.view.CarWashView;
 
 public class Simulator extends Observable {
-	
+
 	/**
 	 * Here we set some variables which is used on other parts of the code.
 	 */
 
-		public static int fastMachines = 2;
-		public static int slowMachines = 2;
-		public static double fastLow = 2.8;
-		public static double fastHigh = 4.6;
-		public static double slowLow = 3.5;
-		public static double slowHigh = 6.7;
-		public static double lambda = 2.0;
-		public static long seed = 1234;
-		public static int queueSize = 5;
-		public static double late = 15.00;
-		
-		public Event event;
-		public static SortedSequence sortSeq;
-		public static CarWashState carWashState;
-		
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
+	public static int fastMachines = 2;
+	public static int slowMachines = 2;
+	public static double fastLow = 2.8;
+	public static double fastHigh = 4.6;
+	public static double slowLow = 3.5;
+	public static double slowHigh = 6.7;
+	public static double lambda = 2.0;
+	public static long seed = 1234;
+	public static int queueSize = 5;
+	public static double late = 15.00;
 
-			carWashState = new CarWashState(queueSize, fastMachines, slowMachines, fastLow, fastHigh, slowLow, slowHigh, lambda, seed);
-			sortSeq = new SortedSequence(carWashState);
-	    	CarWashView carWashView = new CarWashView();
-	    	carWashState.addObserver(carWashView);
-	    	sortSeq.startQueue(new Start(0));
-		}
+	public Event event;
+	public static SortedSequence sortSeq;
+	public static CarWashState carWashState;
+
+	public static void main(String[] args) {
+
+		carWashState = new CarWashState(queueSize, fastMachines, slowMachines,
+				fastLow, fastHigh, slowLow, slowHigh, lambda, seed);
+		sortSeq = new SortedSequence(carWashState);
+		CarWashView carWashView = new CarWashView();
+
+		carWashState.addObserver(carWashView);
+		sortSeq.startQueue(new Start(0)); // Starts the simulation.
 	}
+}
